47f9f582b37e86547b7be7f497f2a0e9fc12d6ba
Binzu <33591057+levinciii@users.noreply.github.com>
2023-09-16 17:35:24 +0800

    Setup Middleware, Routes, and .env

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // BIN-API
 const bin = express()
 
+// Middleware
+bin.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// Routes
+bin.get('/', (req, res) => {
+    res.json({message: 'Welcome to BIN-API'})
+})
+
 // Listen for requests
-bin.listen(2609, () => {
-    console.log('Listening on port 2609.')
+bin.listen(process.env.PORT, () => {
+    console.log('Listening on port', process.env.PORT)
 })
