690c86406364907808b64c2bd9118ed0b652a556
Binzu <33591057+levinciii@users.noreply.github.com>
2024-01-10 14:59:26 +0800

    Clean Code
    - rename some codes in some lines.

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,14 +10,12 @@ const signupUser = async (req, res) => {
     const { email, password } = req.body
 
     try {
-        const user = await User.signUp(email, password)
+        const user = await User.signup(email, password)
 
         res.status(200).json({email, user})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
-
-    res.json({mssg: 'Signup successful!'})
 }
 
 module.exports = {
