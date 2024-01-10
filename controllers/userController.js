3e180df301d52c3b02df2da44d534ffacfeba2cf
Binzu <33591057+levinciii@users.noreply.github.com>
2024-01-10 14:43:57 +0800

    Update 'userController.js' and 'userModel.js'
    - create sign up and hash passwords for security purposes.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,22 @@
 const User = require('../models/userModel')
 
 // LogIn User
-const loginUser = async (req, res) => {
+const loginUser = async (reg, res) => {
     res.json({mssg: 'Login successful!'})
 }
 
 // SignUp User
-const signupUser = async (reg, res) => {
+const signupUser = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.signUp(email, password)
+
+        res.status(200).json({email, user})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
     res.json({mssg: 'Signup successful!'})
 }
 
