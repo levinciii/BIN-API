0b42f208a048cedb373336c8b0e976d3b72e85e7
Binzu <33591057+levinciii@users.noreply.github.com>
2024-01-12 18:57:22 +0800

    Update 'userModel.js'
    - add validation on login and signup pages.

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,16 +18,46 @@ const userSchema = new Schema({
 
 // Static SignUp Method
 userSchema.statics.signup = async function (email, password) {
-    const exists = await this.findOne({ email })
+    // Validation
+    if (!email || !password) {
+        throw Error('All fields cannot be empty!')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Invalid Email!')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password is weak!')
+    }
 
+    const exists = await this.findOne({ email })
     if (exists) {
-        throw Error('Email already in use')
+        throw Error('Email is already existing!')
     }
 
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
     const user = await this.create({ email, password: hash })
+
+    return user
+}
+
+// Static LogIn Method
+userSchema.statics.login = async function(email, password) {
+    // Validation
+    if (!email || !password) {
+        throw Error('All fields cannot be empty!')
+    }
+
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error('Email does not exist!')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+        throw Error('Incorrect Password!')
+    }
 
     return user
 }
